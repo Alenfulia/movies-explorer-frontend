4cb4e595f7e4215c87d9e94f758dd77c1e1ecbff
Alenfulia <alfenkaa@yandex.ru>
2022-06-09 13:04:05 +0300

    Добавление main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import Header from '../Header/Header';
-import Promo from '../Promo/Promo';
-import AboutProject from '../AboutProject/AboutProject';
-import Techs from '../Techs/Techs';
-import AboutMe from '../AboutMe/AboutMe';
-import Portfolio from '../Portfolio/Portfolio'
-import Footer from '../Footer/Footer';
+import Main from '../Main/Main';
+
 import './App.css';
 
 function App() {
   return (
     <>
-    <Header />
-    <Promo />
-    <AboutProject />
-    <Techs />
-    <AboutMe />
-    <Portfolio />
-    <Footer />
+    <Main />
     </>
   );
 }
