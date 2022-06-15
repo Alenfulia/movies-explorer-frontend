11e80445bb7bf7117dc80fa336736372b8afd710
Alenfulia <alfenkaa@yandex.ru>
2022-06-16 00:11:00 +0300

    Добавление savedMovies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,9 +4,11 @@ import { Route, Switch } from 'react-router-dom';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
 import Footer from '../Footer/Footer';
 
 import './App.css';
+
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
     <Switch>
       <Route path='/movies'>
         <Movies />
+      </Route>
+      <Route path='/saved-movies'>
+        <SavedMovies />
       </Route>
       <Route path='/' exact>
         <Main />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,40 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Logo from '../Logo/Logo';
 import Navigation from '../Navigation/Navigation';
 import NavigationLoggedIn from '../NavigationLoggedIn/NavigationLoggedIn';
 
 import './Header.css';
 
-const Header = ({ loggedIn }) => {
+const Header = ({loggedIn}) => {
 
-  return (
-    //  !loggedIn ? (
-    //    <header className='header header-promo'>
-    //      <Logo />
-    //      <Navigation />
-    //      </header>
-    //  ) : (
-      <header className='header header-login'>
+  const location = useLocation();
+
+  if (location.pathname === '/' || location.pathname === '/saved-movies' ||
+  location.pathname === '/movies' || location.pathname === '/profile') {
+    return (
+      <header className={`header ${(!loggedIn)? 'header-promo': ''}`}>
         <Logo />
-        <NavigationLoggedIn />
+        <Navigation />
       </header>
     )
-  //)
+  } else if (location.pathname === '/' || location.pathname === '/saved-movies' ||
+    location.pathname === '/movies' || location.pathname === '/profile') {
+      return (
+        <header className={`header ${(loggedIn)? 'header-login': ''}`}>
+          <Logo />
+          <NavigationLoggedIn />
+        </header>
+      )
+  } else if (location.pathname === '/signup' || location.pathname === '/signin') {
+    return (
+      <header className='header header-auth'>
+        <Logo />
+      </header>
+    )
+  }
+  return null
 }
 
 export default Header;
