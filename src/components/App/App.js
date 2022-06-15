562a796242524317fe23cbd7da3586b377a44c53
Alenfulia <alfenkaa@yandex.ru>
2022-06-16 00:46:18 +0300

    Добавление not found

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import NotFound from '../NotFound/NotFound';
 import Footer from '../Footer/Footer';
 
 import './App.css';
@@ -23,6 +24,9 @@ function App() {
       </Route>
       <Route path='/' exact>
         <Main />
+      </Route>
+      <Route path='*'>
+        <NotFound />
       </Route>
     </Switch>
     <Footer />
